08b79dd132ba394530eb7ceb25e196f53f4ca9c4
mahdi <mahdi13991234@gmail.com>
2021-10-04 00:58:58 +0330

    set state in react js

--- first-project/src/App.js
+++ first-project/src/App.js
@@ -1,14 +1,60 @@
-import React from "react";
+import react from "react";
+import React, { useState } from "react";
 import Product from "./components/Product/Product";
+// class App extends react.Component {
+//    state = {
+//     Products : [
+//       {title: "vuejs" , price: "99 $"},
+//       {title: "nodejs" , price: "120 $"},
+//       {title: "reactjs" , price: "150 $"}
+//     ]
+//   };
+//   clickhandler = () =>{
+//     this.setState({
+//       Products : [
+//         {title: "html" , price: "79 $"},
+//         {title: "css" , price: "89 $"},
+//         {title: "javascript" , price: "99 $"}
+//       ]
+//     })
+//   }
+//  render(){
+//   return (
+//     <div className="container" id="title">
+//       <h1>mahdi</h1>
+//       {this.state.Products.map(product => {
+//         return <Product
+//         name={product.title}
+//         price={product.price} />
+//       })}
+//       <button type="button" onClick={this.clickhandler}>change</button>
+//     </div>
+//   )
+//  }
+// };
+
 const App = () => {
+  const [Products, setProducts] = useState([
+    { title: "vuejs", price: "99 $" },
+    { title: "nodejs", price: "120 $" },
+    { title: "reactjs", price: "150 $" },
+  ]);
+  const clickHandler = () => {
+    setProducts([
+      { title: "html", price: "99 $" },
+      { title: "css", price: "120 $" },
+      { title: "javacript", price: "200 $" },
+    ]);
+  };
   return (
     <div className="container" id="title">
       <h1>mahdi</h1>
-        <Product name="reactjs" price="100 $" />
-        <Product name="nodejs"  price="250 $" />
-        <Product name="vuejs" price="40 $">
-          <p>discout is 15%</p>
-          </Product>
+      {Products.map((product) => {
+        return <Product name={product.title} price={product.price} />;
+      })}
+      <button type="button" onClick={clickHandler}>
+        change
+      </button>
     </div>
   );
 };
